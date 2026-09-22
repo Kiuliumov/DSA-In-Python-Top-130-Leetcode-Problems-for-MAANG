48245d87c4481d60ab0f8512ddabfd35b2733177
Kiuliumov <miti.bania@gmail.com>
2026-09-22 15:48:39 +0300

    Add solutions

--- Solutions/S03 - Arrays and Lists/Max consecutive ones.py
+++ Solutions/S03 - Arrays and Lists/Max consecutive ones.py
@@ -1,1 +1,11 @@
-
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: list[int]) -> int:
+        current_streak = 0
+        max_streak = 0
+        for n in nums:
+            if n == 1:
+                current_streak += 1
+                max_streak = current_streak if current_streak > max_streak else max_streak
+                continue
+            current_streak = 0
+        return max_streak
