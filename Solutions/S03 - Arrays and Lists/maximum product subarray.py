95c9e8647be4bf698944dd77775ebde4ef8b42c5
Kiuliumov <miti.bania@gmail.com>
2026-09-22 18:04:44 +0300

    Solve maximum product subarray

--- Solutions/S03 - Arrays and Lists/maximum product subarray.py
+++ Solutions/S03 - Arrays and Lists/maximum product subarray.py
@@ -1,10 +1,16 @@
 class Solution:
-    def maxProductSubArray(self, nums: list[int]) -> int:
-        s = nums[0]
-        m = nums[0]
+    def maxProduct(self, nums: list[int]) -> int:
+        cur_max = nums[0]
+        cur_min = nums[0]
+        result = nums[0]
 
-        for i in range(1, len(nums)):
-            s = max(nums[i], s * nums[i])
-            m = max(m, s)
+        for x in nums[1:]:
+            if x < 0:
+                cur_max, cur_min = cur_min, cur_max
 
-        return m
+            cur_max = max(x, cur_max * x)
+            cur_min = min(x, cur_min * x)
+
+            result = max(result, cur_max)
+
+        return result
